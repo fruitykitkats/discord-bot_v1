28e6e54e1bd87b38fa0323554ab8ec432eb87630
FruityKitKats <dragonminner867@gmail.com>
2022-10-11 11:03:24 +0200

    some cleaning

--- commands/help.js
+++ commands/help.js
@@ -1,12 +1,11 @@
-// Copyright (c) 2020 FruityKitKats. All rights reserved.
+// Copyright (c) 2017 FruityKitKats. All rights reserved.
 
 module.exports.run = async (bot, message, args) => { 
-
-    var fs = require("fs"); 
-    var commandsList = fs.readFileSync("Storage/commands.txt", "utf8"); 
-
-        message.channel.send(commandsList) 
+    var fs = require('fs'); 
+    var commandsList = fs.readFileSync('Storage/commands.txt', 'utf8'); 
+    message.channel.send(commandsList) 
+}
 
 module.exports.config = { 
-    command: "help"
-}
+    command: 'help'
+};
--- commands/ping.js
+++ commands/ping.js
@@ -1,16 +1,13 @@
-// Copyright (c) 2020 FruityKitKats. All rights reserved.
+// Copyright (c) 2017 FruityKitKats. All rights reserved.
 
 module.exports.run = async (bot, message, args) => { 
-    
         message.channel.send({embed:{ 
-            title:"Ping!",
-            description:"Pong!",
+            title:'Ping!',
+            description:'Pong!',
             color: 0x5DADE2   
         }}) 
-
     }
 
-
 module.exports.config = { 
-    command: "ping"
-}
+    command: 'ping'
+};
